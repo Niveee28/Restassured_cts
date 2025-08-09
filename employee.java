83757d0a875f2f4cbc4f61c2dc4e990f45ea121b
Niveee28 <nivetha28n@gmail.com>
2025-08-09 14:10:04 +0530

    my first commit message

---
+++ employee.java
@@ -0,0 +1,22 @@
+package java2;
+
+public class employee extends personn{
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       employee n=new employee();
+       n.name="nivetha";
+       n.age=21;
+       n.socialSecurityNumber = 32;
+       n.address= "11 thirucholai back street";
+
+       System.out.println(n.name);
+       System.out.println(n.age);
+       System.out.println(n.socialSecurityNumber);
+       System.out.println(n.address);
+       
+       
+       
+	}
+
+}
